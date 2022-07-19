3b87c6c4ac8df864e1c816da1d7ca02435af1afb
Victoria <vickimarji@icloud.com>
2022-07-19 12:08:11 -0400

    new

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
+
+
+
+
+
 function createParagraph() {
-   
+    
     var wordOne = document.getElementById("textOne").value;
     var wordTwo = document.getElementById("textTwo").value;
     var wordThree = document.getElementById("textThree").value;
@@ -14,10 +19,12 @@ function createParagraph() {
     var wordTwelve = document.getElementById("textTweleve").value;
     var wordThirteen = document.getElementById("textThirteen").value;
 
- 
 
-    var paragraph = "It was a " + wordOne + " cold November day.  I woke up " + wordTwo + " smell of " + wordThree + " roasting in the " + wordFour + " downstairs. I " + wordFive + "down the stairs to see if I could help " + wordSix + " the dinner. My mom said See if " + wordSeven + " needs a fresh " + wordEight + " So I carried  tray of glasses full of " + wordNine + " into the " + wordTen + " room. " + " When I got there I couldn't believe my " + wordEleven +! + " There were "  + wordTwelve + " on the " + wordThirteen +"!";
+    console.log(wordOne,wordTwo,wordThree,wordFour,wordFive,wordSix,wordSeven,wordEight,wordNine,wordTen,wordEleven,wordTwelve, wordThirteen)
 
+    var paragraph = "It was a " + wordOne + " cold November day  I woke up " + wordTwo + " smell of " + wordThree + " roasting in the " + wordFour + "downstairs. I " + wordFive + " down the stairs to see if I could help " + wordSix + " the dinner. My mom said See if " + wordSeven + " needs a fresh " + wordEight + " So I carried  tray of glasses full of " + wordNine + " into the " + wordTen + " room.  When I got there I couldn't believe my " + wordEleven + " There were "  + wordTwelve + " on the " + wordThirteen + "!";
+
+    
 
     document.getElementById('answer').innerHTML = paragraph;
 }
